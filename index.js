640469711507f3a13397a882bc1f30fba555b50a
suttipong <60011087@kmitl.ac.th>
2020-04-12 03:49:26 +0700

    done keypoints ratio

--- index.js
+++ index.js
@@ -7,29 +7,83 @@ function gotPose(poses) {
     console.log(poses);
 }
 let capture;
-let tmp;
+let poses_;
 
 function setup() {
-    createCanvas(480, 480);
+    createCanvas(650, 600);
     capture = createCapture(VIDEO);
     capture.hide();
     poseNet = ml5.poseNet(capture, modelLoaded);
     poseNet.on('pose', (poses) => {
-        tmp = poses[0].pose.keypoints;        
-        // console.log(tmp);
+        // tmp = poses[0].pose.keypoints;   
+
+        for(let i = 0 ; i < poses.length ;i++){
+            poses_ = poses[i];
+            // console.log(poses_);
+            if (poses_) {
+
+                let pose = poses_.pose;
+              
+                //mid point
+                let midpose = average2point(pose.leftShoulder,pose.rightShoulder); 
+    
+                // shoulder lenght
+                let shoulderLenght = distance(pose.leftShoulder,pose.rightShoulder);
+        
+                let list_ = [];
+        
+                for(let i = 0 ; i < pose.keypoints.length ;i++){
+                    let point = pose.keypoints[i].position;
+                    let dist = distance(point,midpose);
+                    list_.push({
+                        dist : dist/shoulderLenght,
+                        score : pose.keypoints[i].score
+                    });
+                    // console.log("point of "+ pose.keypoints[i].part + " " + dist)
+            
+                }
+        
+                for(let i = 0 ; i < list_.length ;i++){
+                    if(list_[i].score >= .50){
+                        console.log(pose.keypoints[i].part + " " + list_[i].dist.toFixed(3) + " " + list_[i].score.toFixed(3));
+                    }
+                }
+            }
+
+        }
+    
     });
 
 }
 
+
+function distance(A,B){
+    return sqrt((A.x-B.x)**2 + (A.y-B.y)**2);
+}
+
+function average2point(A,B){
+    return {
+        x : (A.x+B.x)/2,
+        y : (A.y+B.y)/2
+    };
+}
+
+
 function draw() {
+    background(20,20,20);
     image(capture, 0, 0);
-    fill(255, 0, 0);
-
-    if (tmp) {
-        console.log(tmp);
-        for (let i = 0; i < 17; i++) {
-            ellipse(tmp[i].position.x, tmp[i].position.y, 10);
+   
+    if (poses_) {
+        let pose = poses_.pose;
+        fill(255, 0, 0);
+        for (let i = 0; i < pose.keypoints.length; i++) {
+            if(pose.keypoints[i].score >= .50){
+                ellipse(pose.keypoints[i].position.x, pose.keypoints[i].position.y, 10);
+            }
         }
-    }
 
+        let midpose = average2point(pose.leftShoulder,pose.rightShoulder);
+        fill(0,0,255);
+        ellipse(midpose.x,midpose.y,10);
+    }
 }
